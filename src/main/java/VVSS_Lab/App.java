36af39d37e690e61ae56c1ba0dcfc75192649318
Stefan Delibas <delibas.stefan@gmail.com>
2019-03-20 10:51:39 +0200

    Initial commit

---
+++ src/main/java/VVSS_Lab/App.java
@@ -0,0 +1,13 @@
+package VVSS_Lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/VVSS_Lab/AppTest.java
@@ -0,0 +1,20 @@
+package VVSS_Lab;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
